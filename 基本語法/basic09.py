7689998e9c1b0f1bb1c7240bea58fcf8df2fdd17
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-27 21:23:56 +0800

    Add files via upload

--- 基本語法/basic09.py
+++ 基本語法/basic09.py
@@ -1,3 +1,4 @@
+import re
 # Python 基本資料結構
 # list   列表[]    (元素內容可以重複, 元素內容可以修改)
 # tuple  列表()    (唯讀, Fast)
@@ -30,3 +31,30 @@ empIds = [1, 3, 5, 2, 3, 1]
 empIds = set(empIds)
 print(type(empIds), empIds)
 print(len(empIds))
+
+# dict 字典列表 (key:value)
+# key 的值不可以重複
+# value 的值可以重複
+a = {'symbol': '2330.TW', 'price': 599}
+b = {'symbol': '2317.TW', 'price': 108}
+c = {'symbol': '3008.TW', 'price': 2080}
+prices = [a, b, c]
+print(type(prices), prices)
+for p in prices:
+    print(p, p.get('symbol'), p.get('price'))
+
+# 切割字串 split
+#股名,價格,殖利率,本益比,股價淨值比
+s = "2330.TW,599,1.67,28.03,7.8"
+s = s.split(",")
+print(type(s), s, '本益比', s[3])
+
+# 多符號切割 (需要 import re)
+s = "2330.TW,599#1.67!28.03,7.8"
+s = re.split(',|#|!', s)
+print(type(s), s, '本益比', s[3])
+
+# split 轉 dict(字典格式 key=value)
+s = "股名=2330.TW,價格=599,殖利率=1.67,本益比=28.03,股價淨值比=7.8"
+s = dict(ex.split("=") for ex in s.split(","))
+print(type(s), s, s.get('本益比'))
