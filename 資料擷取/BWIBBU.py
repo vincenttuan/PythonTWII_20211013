0ae8df54cdd8f4e3cb4d7511007506243a055a95
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-03 20:45:05 +0800

    Add files via upload

--- 資料擷取/BWIBBU.py
+++ 資料擷取/BWIBBU.py
@@ -1,7 +1,9 @@
 import requests
 
-def getData():
-    url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20211103&selectType=ALL'
+def getData(yyyy, mm, dd):
+    yyyymmdd = str(yyyy) + str(mm) + str(dd)
+    url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=%s&selectType=ALL' % yyyymmdd
+    print(url)
     data = requests.get(url).text
     data = data.split("\r\n")
     # print(type(data), data)
@@ -16,11 +18,11 @@ def getData():
                     float(array[2]) if array[2] != '-' else -1,
                     float(array[4]) if array[4] != '-' else -1,
                     float(array[5]) if array[5] != '-' else -1,
-                    '2021-11-03'
+                    str(yyyy) + "-" + str(mm) + "-" + str(dd)
                 )
             list.append(row)
     return list
 
 if __name__ == '__main__':
-    list = getData()
+    list = getData(2021, 11, 3)
     print(list)
