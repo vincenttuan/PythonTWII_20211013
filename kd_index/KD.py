089b0716afa31324a62003c5c13a09c1aeaed920
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-24 20:02:55 +0800

    Add files via upload

--- kd_index/KD.py
+++ kd_index/KD.py
@@ -22,5 +22,26 @@ if __name__ == '__main__':
     # 將資料讀進到 DataFrame
     tx = pd.read_sql(sql, conn)
     tx = tx.set_index('date')
-    print(tx)
-
+    #print(tx)
+    print('---------------------------------------------------------------------------')
+    # RSV
+    # RSV = (今日收盤價 - 最近9天的最低價) / (最近9天的最高價 - 最近9天的最低價)
+    # 計算 9 日內的最高成交價
+    tx['9dMax'] = tx['最高價'].rolling(9).max()
+    # 計算 9 日內的最低成交價
+    tx['9dMin'] = tx['最低價'].rolling(9).min()
+    # 刪除 NaN 資料列
+    tx = tx.dropna()
+    #print(tx)
+    print('---------------------------------------------------------------------------')
+    tx['RSV'] = 0
+    tx['RSV'] = 100 * (tx['收盤價'] - tx['9dMin']) / (tx['9dMax'] - tx['9dMin'])
+    #print(tx)
+    print('---------------------------------------------------------------------------')
+    # apply() 的使用
+    def add(n):
+        return n + 3
+    df = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
+    print(df)
+    df['new_y'] = df['y'].apply(add)
+    print(df)
