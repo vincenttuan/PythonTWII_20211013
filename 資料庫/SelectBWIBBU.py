e9c609df8ce8b6cfb4ca680f16f800cc266f0853
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-01 16:21:29 +0800

    Delete 資料庫 directory

--- 資料庫/SelectBWIBBU.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('tw_stock.db')
-cursor = conn.cursor()
-sql = 'select * from BWIBBU where pe > 0 and pe < 20 and pb < 1 and yield > 10'
-cursor.execute(sql)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
