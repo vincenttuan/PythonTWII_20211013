2f5f2d9d97fc4c42c9aa05ee24fc02c745cfe53e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-01 20:57:53 +0800

    Add files via upload

---
+++ 一籃子股票/一籃子選股_PB_當月營收.py
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+# 一籃子股票指數:
+# 股價淨值比 0.1 < pb < 0.5
+# 近三個月營收 > 近一年營收
+# 回測驗證
+# 計算 ROI 報酬率
+
+if __name__ == '__main__':
+    # 這一天需要有交易
+    tday = datetime.date(2021, 1, 4)
+    conn = sqlite3.connect('../資料庫/財經資料庫.db')
+    sql = '''
+           SELECT stock_id, date, 收盤價 FROM price
+           where date >= '%s'
+           ''' % (tday)
+    print(sql)
+    # date -> index, stock_id -> columns, 收盤價 -> data
+    price = pd.read_sql(sql, conn, parse_dates=['date']).pivot(index='date', columns='stock_id')['收盤價']
+    print(price)
+    
