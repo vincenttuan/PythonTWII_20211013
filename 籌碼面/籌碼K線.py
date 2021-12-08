500e9a4d075f5e5127d7220918f3bab1159b2700
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-08 20:08:42 +0800

    Update 籌碼K線.py

--- 籌碼面/籌碼K線.py
+++ 籌碼面/籌碼K線.py
@@ -1,8 +1,14 @@
+import time
 import requests
 import re
+import requests.packages.urllib3
+
+requests.packages.urllib3.disable_warnings()
+headers = {'User-Agent': 'chrome'}
+
+# 取得驗證碼
 def get_code(rs):
-    headers = {'User-Agent': 'chrome'}
-    res = rs.get('http://bsr.twse.com.tw/bshtm/bsMenu.aspx', headers=headers)
+    res = rs.get('http://bsr.twse.com.tw/bshtm/bsMenu.aspx', verify=False, stream=True, headers=headers, timeout=None)
     #print(res.text)
     # 透過 re 正則表示式來抓取
     viewstate = re.search('__VIEWSTATE"\s+value=.*=', res.text)
@@ -18,9 +24,35 @@ def get_code(rs):
     eventvalidation = eventvalidation.group(0)[26:]
 
     return viewstate, eventvalidation
+
+# 傳送資料（表單）
+def send_data(rs, stock_id, viewstate, eventvalidation):
+    payload = {
+        '__EVENTTARGET': '',
+        '__EVENTARGUMENT': '',
+        '__LASTFOCUS': '',
+        '__VIEWSTATE': viewstate,  # encode_viewstate[:-1],
+        '__EVENTVALIDATION': eventvalidation,  # encode_eventvalidation[:-1],
+        'RadioButton_Normal': 'RadioButton_Normal',
+        'TextBox_Stkno': stock_id,
+        'CaptchaControl1 ': 'Z67YB',
+        'btnOK': '%E6%9F%A5%E8%A9%A2',
+    }
+
+    # 模擬按下「查詢」鍵
+    rs.post("https://bsr.twse.com.tw/bshtm/bsMenu.aspx", data=payload, headers=headers, stream=True)
+    # 停 1 秒
+    time.sleep(1)
+    res = rs.get('https://bsr.twse.com.tw/bshtm/bsContent.aspx', verify=False, stream=True)
+    return res
+
 if __name__ == '__main__':
     rs = requests.session()
     viewstate, eventvalidation = get_code(rs)
     print('viewstate = ' + viewstate)
     print('eventvalidation = ' + eventvalidation)
+
+    res = send_data(rs, '1101', viewstate, eventvalidation)
+    res.encoding = 'big5'
+    open('test.html', 'w', encoding='utf-8').write(res.text)
 
